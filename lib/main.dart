b12c9ffbf06bb36af591baf249b2d238ae4260e0
StarDustShining <949743751@qq.com>
2024-06-14 00:30:40 +0800

    090

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DOD',
+      title: '命运飞镖',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
