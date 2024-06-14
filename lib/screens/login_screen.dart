ff1832b00960087a782d9979d2df74c0372762a2
StarDustShining <949743751@qq.com>
2024-06-14 17:25:46 +0800

    56

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -54,6 +54,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Form(
           key: _formKey,
           child: Column(
+            padding:const EdgeInsets.all(16.0),
             children: [
               CustomTextField(
                 controller: _usernameController,
